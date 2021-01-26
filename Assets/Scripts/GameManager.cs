b180eb0244d27f4fd1a0f5578ab195b910fffa15
geoffatSAE <g.hill@sae.edu>
2021-01-27 10:13:13 +1100

    Initial commit of Unity Project.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public float timer;
+    public bool goalReached;
+    public Text uiText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!goalReached)
+        {
+            timer += Time.deltaTime;
+            uiText.text = timer.ToString("F0");
+        } else
+        {
+            //GAME OVER
+            Debug.Log("GAME OVER MAN, GAME OVER");
+        }
+        
+
+
+
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            goalReached = true;
+        }
+    }
+}
---
+++ Assets/Scripts/TorchScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchScript : MonoBehaviour
+{
+
+    public Light l;
+    float timer = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        l = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        timer += Time.deltaTime;
+
+        if (timer >= 0.2)
+        {
+            l.intensity = Random.Range(0.1f, 1.0f);
+            timer = 0.0f;
+        }
+
+
+
+
+    }
+}
